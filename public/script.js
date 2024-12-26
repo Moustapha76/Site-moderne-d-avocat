3129126fee079b2448f83f78b37d438c90247154
Mouhamadou DIOUF <m.diouf@futurdigital.local>
2024-12-26 18:22:17 +0100

    last update

--- public/script.js
+++ public/script.js
@@ -32,6 +32,20 @@ var swiperService = new Swiper(".services-list", {
     }
 });
 
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
+
 // Accordion
 const items = document.querySelectorAll(".accordion button");
 
