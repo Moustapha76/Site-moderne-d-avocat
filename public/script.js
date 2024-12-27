c61ca95407dd86f99505b2ccf497f70fec241efe
Mouhamadou DIOUF <m.diouf@futurdigital.local>
2024-12-27 17:22:13 +0100

    version3

--- public/script.js
+++ public/script.js
@@ -1,65 +1,71 @@
-// Menu mobile
-let btnmenu = document.querySelector('header .burger')
-let closeBtn = document.querySelector('header .closeBtn')
-let menulist = document.querySelector('header nav ul')
-btnmenu.addEventListener('click', function(){
-    menulist.classList.toggle('opened')
-})
-closeBtn.addEventListener('click', function(){
-    menulist.classList.toggle('opened')
-})
+document.addEventListener('DOMContentLoaded', function(){
+  setTimeout(function(){
+    document.querySelector('.preloader').style.display = 'none';
+  }, 1000)
+  // Menu mobile
+  let btnmenu = document.querySelector('header .burger')
+  let closeBtn = document.querySelector('header .closeBtn')
+  let menulist = document.querySelector('header nav ul')
+  btnmenu.addEventListener('click', function(){
+      menulist.classList.toggle('opened')
+  })
+  closeBtn.addEventListener('click', function(){
+      menulist.classList.toggle('opened')
+  })
 
-// Navbar
-let navbar = document.querySelector('.navbar')
-document.addEventListener('scroll',function(){
-    if(window.scrollY>0){navbar.classList.add('fixed')}
-    else{navbar.classList.remove('fixed')}
-}) 
+  // Navbar
+  let navbar = document.querySelector('.navbar')
+  document.addEventListener('scroll',function(){
+      if(window.scrollY>0){navbar.classList.add('fixed')}
+      else{navbar.classList.remove('fixed')}
+  }) 
 
 
-var swiperService = new Swiper(".services-list", {
-    slidesPerView: 3,
-    autoplay: false,
-    speed: 1000,
+  var swiperService = new Swiper(".services-list", {
+      slidesPerView: 3,
+      autoplay: false,
+      speed: 1000,
+      loop: true,
+      spaceBetween: 50, //init: false,
+      navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+      breakpoints: {
+          1024: { slidesPerView: 2},
+          768: { slidesPerView: 1},
+          640: { slidesPerView: 1},
+          320: { slidesPerView: 1}
+      }
+  });
+
+  var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
     loop: true,
-    spaceBetween: 50, //init: false,
-    navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
-    breakpoints: {
-        1024: { slidesPerView: 2},
-        768: { slidesPerView: 1},
-        640: { slidesPerView: 1},
-        320: { slidesPerView: 1}
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
     }
-});
+  });
 
-var swiper = new Swiper('.blog-slider', {
-  spaceBetween: 30,
-  effect: 'fade',
-  loop: true,
-  mousewheel: {
-    invert: false,
-  },
-  // autoHeight: true,
-  pagination: {
-    el: '.blog-slider__pagination',
-    clickable: true,
+  // Accordion
+  const items = document.querySelectorAll(".accordion button");
+
+  function toggleAccordion() {
+    const itemToggle = this.getAttribute('aria-expanded');
+    
+    for (i = 0; i < items.length; i++) {
+      items[i].setAttribute('aria-expanded', 'false');
+    }
+    
+    if (itemToggle == 'false') {
+      this.setAttribute('aria-expanded', 'true');
+    }
   }
-});
 
-// Accordion
-const items = document.querySelectorAll(".accordion button");
+  items.forEach(item => item.addEventListener('click', toggleAccordion));
+  // Accordion
 
-function toggleAccordion() {
-  const itemToggle = this.getAttribute('aria-expanded');
-  
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
-  }
-  
-  if (itemToggle == 'false') {
-    this.setAttribute('aria-expanded', 'true');
-  }
-}
-
-items.forEach(item => item.addEventListener('click', toggleAccordion));
-// Accordion
+})
